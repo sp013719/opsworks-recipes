7501d46db5057033a33e51a064677cd700d4eb64
a-hidetos <hideto_saito@trendmicro.com>
2014-09-09 14:50:08 -0700

    align to Amazon Linux
    - no “freeipmi”
    - no "samba-client”
    - no "system-config-network-tui"

--- BL-1/recipes/basepkgs.rb
+++ BL-1/recipes/basepkgs.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{audit augeas-libs autofs bash bind-utils bzip2 bzip2-libs compat-libcap1 crontabs csh device-mapper-multipath dhclient dmidecode diffutils e2fsprogs e4fsprogs emacs freeipmi grub iscsi-initiator-utils kernel krb5-libs krb5-workstation libselinux-ruby logrotate lsof mailx man module-init-tools nc net-snmp net-snmp-libs net-snmp-utils net-snmp-perl nfs-utils nfs-utils-lib nmap ntp OpenIPMI openssh openssh-clients openssh-server pam_passwdqc parted portmap postfix psacct rsync rsyslog ruby rubygems ruby-libs screen samba-client system-config-network-tui sudo strace sysstat tcpdump telnet traceroute unzip vim-enhanced virt-what wget which yum zsh}.each do |p|
+%w{audit augeas-libs autofs bash bind-utils bzip2 bzip2-libs compat-libcap1 crontabs csh device-mapper-multipath dhclient dmidecode diffutils e2fsprogs e4fsprogs emacs grub iscsi-initiator-utils kernel krb5-libs krb5-workstation libselinux-ruby logrotate lsof mailx man module-init-tools nc net-snmp net-snmp-libs net-snmp-utils net-snmp-perl nfs-utils nfs-utils-lib nmap ntp OpenIPMI openssh openssh-clients openssh-server pam_passwdqc parted portmap postfix psacct rsync rsyslog ruby rubygems ruby-libs screen sudo strace sysstat tcpdump telnet traceroute unzip vim-enhanced virt-what wget which yum zsh}.each do |p|
 	package p do
 		action :install
 	end
