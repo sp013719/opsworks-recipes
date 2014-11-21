1fe7c5f3f17ee5191f4c1d30b00b32b3bbe38468
a-hidetos <hideto_saito@trendmicro.com>
2014-11-21 12:34:26 -0800

    workaround

--- BL-1/recipes/omelet.rb
+++ BL-1/recipes/omelet.rb
@@ -1,6 +1,4 @@
-include_recipe "python"
-
-python_pip "omelet" do
-    source "git+https://github.com/TrendMicroDCS/omelet.git@dcs-hybridcloud#egg=omelet"
+python_pip " " do
+    options "-e git+https://github.com/TrendMicroDCS/omelet.git@dcs-hybridcloud#egg=omelet"
     action :install
 end
