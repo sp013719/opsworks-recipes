eeb8ead5d565fc94004f1a802df4f65caf6d7c13
a-hidetos <hideto_saito@trendmicro.com>
2014-11-21 13:01:34 -0800

    collecting an argument

--- BL-1/recipes/omelet.rb
+++ BL-1/recipes/omelet.rb
@@ -1,5 +1,5 @@
 execute "pip" do
-    command "/usr/bin/pip git+https://github.com/TrendMicroDCS/omelet.git@dcs-hybridcloud#egg=omelet"
+    command "/usr/bin/pip install --upgrade git+https://github.com/TrendMicroDCS/omelet.git@dcs-hybridcloud#egg=omelet"
     user "root"
     action :run
 end
