6ff5060548e4d151e71f8019813ec4578ae94d44
carol-hsu <asymplone@gmail.com>
2014-11-27 16:03:02 +0800

    new tagging method

---
+++ custom-tag/metadata.rb
@@ -0,0 +1,6 @@
+name 	"custom-tag"
+version "0.0.1"
+
+depends 'aws'
+provides 'custom-tag::default'
+
--- custom-tag/recipes/default.rb
+++ custom-tag/recipes/default.rb
@@ -1,24 +1,8 @@
-require "net/http"
-require "uri"
+include_recipe "aws"
 
-#get instance-id
-uri = URI.parse("http://169.254.169.254/latest/meta-data/instance-id")
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-response = http.request(request)
-instance_id = response.body
-
-
-node[:dcs][:customtags].each do |tagKey, tagValue|
-	execute "ec2tagdynamic" do
-	   command ". /etc/profile && /opt/aws/bin/ec2tag #{instance_id} -t #{tagKey}=#{tagValue}"
-	   action :run
-	   ignore_failure true
+unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
+	aws_resource_tag node['ec2']['instance_id'] do
+    	tags(node['aws-tag']['tags'])
+    	action :update
 	end
-end
-
-execute "ec2tagstatic" do
-   command ". /etc/profile && /opt/aws/bin/ec2tag #{instance_id} -t owner=hideto"
-   action :run
-   ignore_failure true
 end
