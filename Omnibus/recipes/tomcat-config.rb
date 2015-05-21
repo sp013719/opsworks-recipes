db1e4407f4441383105fcddb82680bfd8a2320bb
carol-hsu <asymplone@gmail.com>
2015-05-21 17:33:52 +0800

    add tomcat config

---
+++ Omnibus/recipes/tomcat-config.rb
@@ -0,0 +1,24 @@
+driver_class = case deploy[:database][:type]
+  when "mysql"
+    'com.mysql.jdbc.Driver'
+  when "postgresql"
+    'org.postgresql.Driver'
+  else
+    ''
+  end
+
+
+template 'tomcat server configuration for Omnibus' do
+	path	'/etc/tomcat7.0/Catalina/localhost/ROOT.xml'
+	source 	'tomcat-context.xml.erb'
+	owner	'tomcat'
+	group	'tomcat'
+	mode	'0640'
+	varibles (
+		:resource_name => node['opsworks_java']['datasources']['root'],
+		:application => 'root',
+		:driver_class => driver_class,
+	)
+	notifies :restart, 'tomcat'
+end
+
