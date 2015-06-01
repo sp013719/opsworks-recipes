8c2dd261638ac8f19074acf2b4229cedb04aadc6
carol-hsu <asymplone@gmail.com>
2015-06-01 15:56:36 +0800

    two tomcat-config recipes

---
+++ Omnibus/recipes/tomcat-config-sv.rb
@@ -0,0 +1,43 @@
+service 'tomcat' do
+    service_name 'tomcat7'
+    supports :restart => true, :reload => true, :status => true
+    action :nothing
+end
+
+#driver_class = case node[:deploy]['root'][:database][:type]
+#  when "mysql"
+#    'com.mysql.jdbc.Driver'
+#  when "postgresql"
+#    'org.postgresql.Driver'
+#  else
+#    ''
+#  end
+#
+#
+#template 'tomcat server configuration for Omnibus' do
+#	path	'/etc/tomcat7/Catalina/localhost/ROOT.xml'
+#	source 	'tomcat-context.xml.erb'
+#	owner	'tomcat'
+#	group	'tomcat'
+#	mode	0640
+#	variables ({
+#		:resource_name => node['opsworks_java']['datasources']['root'],
+#		:application => 'root',
+#		:driver_class => driver_class
+#	})
+#	notifies :restart, 'service[tomcat]'
+#end
+
+
+bash 'tomcat-resource-config' do
+	user 'root'
+	cwd '/etc/tomcat7/Catalina/localhost/'
+	code <<-EOH
+	sed -i '5c \\            maxActive=\"30\" maxIdle=\"5\" maxWait=\"1000\"' ROOT.xml
+	sed -i '5a \\            validationQuery=\"SELECT 1\" testOnBorrow=\"true\"' ROOT.xml
+	sed -i '6a \\            removeAbandoned=\"true\" removeAbandonedTimeout=\"30\"' ROOT.xml
+	chown tomcat ROOT.xml
+	EOH
+	notifies :restart, 'service[tomcat]'
+end
+
--- Omnibus/recipes/tomcat-config.rb
+++ Omnibus/recipes/tomcat-config.rb
@@ -4,40 +4,27 @@ service 'tomcat' do
     action :nothing
 end
 
-#driver_class = case node[:deploy]['root'][:database][:type]
-#  when "mysql"
-#    'com.mysql.jdbc.Driver'
-#  when "postgresql"
-#    'org.postgresql.Driver'
-#  else
-#    ''
-#  end
-#
-#
-#template 'tomcat server configuration for Omnibus' do
-#	path	'/etc/tomcat7/Catalina/localhost/ROOT.xml'
-#	source 	'tomcat-context.xml.erb'
-#	owner	'tomcat'
-#	group	'tomcat'
-#	mode	0640
-#	variables ({
-#		:resource_name => node['opsworks_java']['datasources']['root'],
-#		:application => 'root',
-#		:driver_class => driver_class
-#	})
-#	notifies :restart, 'service[tomcat]'
-#end
+driver_class = case node[:deploy]['root'][:database][:type]
+  when "mysql"
+    'com.mysql.jdbc.Driver'
+  when "postgresql"
+    'org.postgresql.Driver'
+  else
+    ''
+  end
 
 
-bash 'tomcat-resource-config' do
-	user 'root'
-	cwd '/etc/tomcat7/Catalina/localhost/'
-	code <<-EOH
-	sed -i '5c \\            maxActive=\"30\" maxIdle=\"5\" maxWait=\"1000\"' ROOT.xml
-	sed -i '5a \\            validationQuery=\"SELECT 1\" testOnBorrow=\"true\"' ROOT.xml
-	sed -i '6a \\            removeAbandoned=\"true\" removeAbandonedTimeout=\"30\"' ROOT.xml
-	chown tomcat ROOT.xml
-	EOH
+template 'tomcat server configuration for Omnibus' do
+	path	'/etc/tomcat7/Catalina/localhost/ROOT.xml'
+	source 	'tomcat-context.xml.erb'
+	owner	'tomcat'
+	group	'tomcat'
+	mode	0640
+	variables ({
+		:resource_name => node['opsworks_java']['datasources']['root'],
+		:application => 'root',
+		:driver_class => driver_class
+	})
 	notifies :restart, 'service[tomcat]'
 end
 
