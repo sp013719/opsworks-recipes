606f114592601f751a2492dc2a2959f9e3219620
hidetosaito <hideto.saito@yahoo.com>
2015-06-10 14:51:48 -0700

    Chef recipes for Kubernetes

---
+++ kubernetes/recipes/etcd.rb
@@ -0,0 +1,23 @@
+bash 'install_etcd' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+  wget https://github.com/coreos/etcd/releases/download/v2.0.11/etcd-v2.0.11-linux-amd64.tar.gz
+  tar zxvf etcd-v2.0.11-linux-amd64.tar.gz
+  cd etcd-v2.0.11-linux-amd64
+  cp etcd etcdctl /usr/local/bin
+  EOH
+end
+
+
+template "/etc/init.d/etcd" do
+	mode "0755"
+	owner "root"
+	source "etcd.erb"
+end
+
+
+service "etcd" do
+	action [:enable, :start]
+	subscribes :reload, "template[/etc/init.d/etcd]", :immediately
+end
