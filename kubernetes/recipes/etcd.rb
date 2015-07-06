5b17af79ee65b122478fead00bb2d4cb489c5966
carol-hsu <asymplone@gmail.com>
2015-07-06 18:13:51 +0800

    add etcd ba

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -21,3 +21,16 @@ service "etcd" do
 	action [:enable, :start]
 	subscribes :reload, "template[/etc/init.d/etcd]", :immediately
 end
+
+#create ba root account and revoke permission of guest
+
+bash "api_enable_auth" do
+	user 'root'
+	code <<-EOH
+		curl -X PUT -d "{\"user\":\"root\",\"password\":\"#{node['ba']['password']}\",\"roles\":[\"root\"]}" http://localhost:4001/v2/auth/users/root
+		curl -X PUT http://localhost:4001/v2/auth/enable
+		AUTHSTR=$(echo -n "root:#{node['ba']['password']}" | base64)
+		curl -H "Authorization: Basic $AUTHSTR" -X PUT -d "{\"role\":\"guest\",\"revoke\":{\"kv\":{\"read\":[\"*\"],\"write\":[\"*\"]}}}" http://localhost:4001/v2/auth/roles/guest
+	EOH
+end
+
