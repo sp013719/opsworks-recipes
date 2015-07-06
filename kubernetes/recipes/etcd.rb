1902cdfde293b8128532581b9b8b1b60800b309a
carol-hsu <asymplone@gmail.com>
2015-07-06 14:33:22 +0800

    update etcd version

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -2,9 +2,9 @@ bash 'install_etcd' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  wget https://github.com/coreos/etcd/releases/download/v2.0.11/etcd-v2.0.11-linux-amd64.tar.gz
-  tar zxvf etcd-v2.0.11-linux-amd64.tar.gz
-  cd etcd-v2.0.11-linux-amd64
+  wget https://github.com/coreos/etcd/releases/download/v2.1.0-rc.0/etcd-v2.1.0-rc.0-linux-amd64.tar.gz
+  tar zxvf etcd-v2.1.0-rc.0-linux-amd64.tar.gz
+  cd etcd-v2.1.0-rc.0-linux-amd64
   cp etcd etcdctl /usr/local/bin
   EOH
 end
