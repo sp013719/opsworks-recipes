56367bbc6baa374e9a5a849bc0dc0e8bcf4f3e20
nosus <carol@tw-carolhsu-2.client.tw.trendnet.org>
2015-07-23 10:52:54 +0800

    add order

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -20,5 +20,19 @@ end
 service "etcd" do
 	action [:enable, :start]
 	subscribes :reload, "template[/etc/init.d/etcd]", :immediately
+	notifies :run, "ba_setup", :immediately
 end
+
+bash 'ba_setup' do
+	user 'root'
+	cwd '/tmp'
+	code <<-EOH
+	curl -X PUT -d "{\"user\":\"root\",\"password\":\"#{node['etcd_password']}\",\"roles\":[\"root\"]}" http://localhost:4001/v2/auth/users/root > ba_log
+	curl -X PUT http://localhost:4001/v2/auth/enable >> ba_log
+	AUTHSTR=$(echo -n "root:#{node['etcd_password']}" | base64)
+	curl -H "Authorization: Basic $AUTHSTR" -X PUT -d "{\"role\":\"guest\",\"revoke\":{\"kv\":{\"read\":[\"*\"],\"write\":[\"*\"]}}}" http://localhost:4001/v2/auth/roles/guest >> ba_log
+	EOH
+	action :nothing
+end
+
 
