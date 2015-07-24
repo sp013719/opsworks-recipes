7efa97dda5f3f7d96aad6080f4ea9160e72bdadf
nosus <carol@tw-carolhsu-2.client.tw.trendnet.org>
2015-07-24 11:01:19 +0800

    move etcd ba to bash file

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -16,23 +16,34 @@ template "/etc/init.d/etcd" do
 	source "etcd.erb"
 end
 
-
 service "etcd" do
 	action [:enable, :start]
 	subscribes :reload, "template[/etc/init.d/etcd]", :immediately
-	notifies :run, "bash[ba_setup]", :delayed
+	subscribes :reload, "template[/root/etcd_enable_ba.sh]", :immediately	
+end
+
+template "/root/etcd_enable_ba.sh" do
+    mode "0755"
+    owner "root"
+    source "etcd_enable_ba.sh.erb"
+    variables :etcd_password => node[:etcd_password]
+    notifies :run, "bash[ba_setup]", :delayed
 end
 
 bash 'ba_setup' do
-	user 'root'
-	cwd '/tmp'
-	code <<-EOH
-	curl -X PUT -d "{\"user\":\"root\",\"password\":\"#{node['etcd_password']}\",\"roles\":[\"root\"]}" http://localhost:4001/v2/auth/users/root > ba_log
-	curl -X PUT http://localhost:4001/v2/auth/enable >> ba_log
-	AUTHSTR=$(echo -n "root:#{node['etcd_password']}" | base64)
-	curl -H "Authorization: Basic $AUTHSTR" -X PUT -d "{\"role\":\"guest\",\"revoke\":{\"kv\":{\"read\":[\"*\"],\"write\":[\"*\"]}}}" http://localhost:4001/v2/auth/roles/guest >> ba_log
-	EOH
-	action :nothing
-end
+    user 'root'
+    cwd '/root'
+    code <<-EOH
+    tries=0
+    while [ $tries -lt 10 ]; do
+        sleep 1
+        tries=$((tries + 1))
+    done
 
+    /root/etcd_enable_ba.sh
+
+    EOH
+
+    action :nothing
+end
 
