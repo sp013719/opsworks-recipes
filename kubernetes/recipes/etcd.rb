c864821f1977c5a7145859c8af825b3c7da66b84
hidetosaito <hideto.saito@yahoo.com>
2015-08-03 17:12:18 +0800

    upgrade back to 2.1.1

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -2,9 +2,9 @@ bash 'install_etcd' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  wget --max-redirect 255 https://github.com/coreos/etcd/releases/download/v2.0.12/etcd-v2.0.12-linux-amd64.tar.gz
-  tar zxvf etcd-v2.0.12-linux-amd64.tar.gz
-  cd etcd-v2.0.12-linux-amd64
+  wget --max-redirect 255 https://github.com/coreos/etcd/releases/download/v2.1.1/etcd-v2.1.1-linux-amd64.tar.gz
+  tar zxvf etcd-v2.1.1-linux-amd64.tar.gz
+  cd etcd-v2.1.1-linux-amd64
   cp etcd etcdctl /usr/local/bin
   EOH
 end
