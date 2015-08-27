8d288693858f0f344f9804307ee98843e41e3810
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-08-27 16:38:24 +0800

    add docker recipe

---
+++ docker/attibutes/default.rb
@@ -0,0 +1,10 @@
+default["docker"] = {}
+
+# docker registry url
+default["docker"]["registry"] = ""
+
+# docker registry auth base64
+default["docker"]["auth"] = ""
+
+# docker registry auth email
+default["docker"]["email"] = "dcsrd@trendmicro.com"
