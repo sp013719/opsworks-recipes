1c9813fe8375336207124ae00d7a27773e3a66f5
nosus <carol_hsu@trend.com.tw>
2015-09-02 19:00:27 +0800

    merge

---
+++ kubernetes/recipes/etcd-clustering.rb
@@ -0,0 +1,33 @@
+service 'etcd' do
+    action :stop
+end
+
+private_ip = node['opsworks']['instance']['private_ip']
+hostname = node['opsworks']['instance']['hostname']
+members = Array.new
+
+node['opsworks']['layers']['etcd']['instances'].each do |inst|
+	members << inst[0]+"=http://"+inst[1][:private_ip]+":7001"
+end
+
+template "/root/etcd_static_bootstrap.sh" do
+	mode "0755"
+	owner "root"
+	source "etcd_static_bootstrap.sh.erb"
+	variables ({
+		:hostname => hostname,
+		:members => members.join(','),
+		:private_ip => private_ip,
+		:token_postfix => node[:token]
+	})
+	notifies :run, "bash[etcd_bootstrap]", :delayed
+end
+
+bash 'etcd_bootstrap' do
+	user 'root'
+	cwd '/root'
+	code <<-EOH
+	rm -rf #{hostname}.etcd
+	./etcd_static_bootstrap.sh
+	EOH
+end
