371f99be83f9d262a580a3e93a831839018a387c
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-09-09 20:11:18 +0800

    update

--- haproxy/recipes/install.rb
+++ haproxy/recipes/install.rb
@@ -1,4 +1,15 @@
 # put the credential
+private_ip = node['opsworks']['instance']['private_ip']
+hostname = node['opsworks']['instance']['hostname']
+instances = Array.new
+
+node['opsworks']['layers']['kub-minion']['instances'].each do |inst|
+	instances << inst[0] inst[1][:private_ip]
+end
+
 template "/root/haproxy" do
   source "haproxy.erb"
+  variables({
+	:instances => instances
+  })
 end
