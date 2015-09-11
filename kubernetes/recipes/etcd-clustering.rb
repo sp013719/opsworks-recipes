6297fc5c087ed4c127d8022b0b0c0e5d0eb9c068
nosus <carol_hsu@trend.com.tw>
2015-09-11 17:52:06 +0800

    add auth meth in clustering

--- kubernetes/recipes/etcd-clustering.rb
+++ kubernetes/recipes/etcd-clustering.rb
@@ -1,9 +1,14 @@
 private_ip = node['opsworks']['instance']['private_ip']
 hostname = node['opsworks']['instance']['hostname']
 members = Array.new
+smallest_ip = 255
 
 node['opsworks']['layers']['etcd']['instances'].each do |inst|
 	members << inst[0]+"=http://"+inst[1][:private_ip]+":7001"
+	last_num = inst[1][:private_ip].split('.')[3]
+	if last_num.to_i < smallest_ip
+		smallest_ip = last_num.to_i
+	end
 end
 
 template "/root/etcd_static_bootstrap.sh" do
@@ -33,3 +38,30 @@ bash 'etcd_bootstrap' do
 	EOH
 	action :nothing
 end
+
+if private_ip.split('.')[3].to_i == smallest_ip
+	template "/root/etcd_enable_ba.sh" do
+	    mode "0755"
+	    owner "root"
+	    source "etcd_enable_ba.sh.erb"
+	    variables :etcd_password => node[:etcd_password]
+	end
+	
+	bash 'ba_setup' do
+	    user 'root'
+	    cwd '/root'
+	    code <<-EOH
+	    tries=0
+	    while [ $tries -lt 10 ]; do
+	        sleep 1
+	        tries=$((tries + 1))
+	    done
+	
+	    /root/etcd_enable_ba.sh
+	
+	    EOH
+	    action :nothing
+	    subscribes :run, "bash[etcd_bootstrap]", :delayed
+	end
+end
+
