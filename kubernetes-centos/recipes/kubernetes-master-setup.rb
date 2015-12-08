77ac9d36c9f27b7effe38786d642ef12cf345042
nosus <carol_hsu@trend.com.tw>
2015-12-08 18:48:39 +0800

    move Berksfile to root

---
+++ kubernetes-centos/recipes/kubernetes-master-setup.rb
@@ -0,0 +1,24 @@
+package ['kubernetes-master', 'kubernetes-client']
+
+template "/etc/init.d/kubernetes-master" do
+	mode "0755"
+	owner "root"
+	source "kubernetes-master.erb"
+	variables({
+		:etcd_url => node['etcd']['elb_url'],
+		:cluster_cidr => node['kubernetes']['cluster_cidr'],
+		:ba_path => "/root/ba_file",
+		:etcd_ba_account => "root",
+		:etcd_ba_password => node['etcd']['password']
+	})
+end
+
+file "/root/ba_file" do
+	owner 'root'
+	group 'root'
+	mode '0600'
+	content "#{node['ba']['password']},#{node['ba']['account']},#{node['ba']['uid']}"
+	action :create
+end
+
+
