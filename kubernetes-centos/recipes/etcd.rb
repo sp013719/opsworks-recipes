2125f452a1a21c9f599d7f3859476041fc3efccf
nosus <carol_hsu@trend.com.tw>
2015-12-08 19:47:22 +0800

    add yum update at beginning

--- kubernetes-centos/recipes/etcd.rb
+++ kubernetes-centos/recipes/etcd.rb
@@ -1,8 +1,14 @@
+execute 'yum-update' do
+	command 'yum -y update'
+	notifies :install, "package[etcd]", :delayed
+end
+
 package 'etcd' do
-	action :install
+	action :nothing
+	notifies :start, "service[etcd]", :delayed
 end
 
 service "etcd" do
-	action [:enable, :start]
+	action :enable
 end
 
