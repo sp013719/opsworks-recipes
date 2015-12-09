31493ce7d6ec4ea423f51052b83cd3079eae21f6
nosus <carol_hsu@trend.com.tw>
2015-12-09 16:45:22 +0800

    update etcd

--- kubernetes-rhel/recipes/repo-setup.rb
+++ kubernetes-rhel/recipes/repo-setup.rb
@@ -2,8 +2,8 @@ bash "enable-repo" do
     user 'root'
     code <<-EOH
 	yum repolist all
-	yum-config-manager —enable rhui-REGION-rhel-server-optional
-	yum-config-manager —enable rhui-REGION-rhel-server-extras
+	yum-config-manager --enable rhui-REGION-rhel-server-optional
+	yum-config-manager --enable rhui-REGION-rhel-server-extras
     EOH
     action :run
 end 
