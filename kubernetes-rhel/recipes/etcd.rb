72da3e48625c6ed7227385f211c35b8dc9b8eb55
nosus <carol_hsu@trend.com.tw>
2015-12-09 19:05:15 +0800

    add auth

--- kubernetes-rhel/recipes/etcd.rb
+++ kubernetes-rhel/recipes/etcd.rb
@@ -2,6 +2,13 @@ include_recipe 'kubernetes-rhel::repo-setup'
 
 package 'etcd' do
 	action :install
+end
+
+template '/etc/etcd/etcd.conf' do
+	source "etcd.conf.erb"
+	mode "0755"
+	owner "root"
+	subscribes :action, "package[etcd]", :delayed
 	notifies :start, "service[etcd]", :delayed
 end
 
