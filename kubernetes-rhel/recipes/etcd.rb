c7a42c36ef3e9c34168967340aa8eedc43df5064
nosus <carol_hsu@trend.com.tw>
2015-12-11 15:47:21 +0800

    update etcd

--- kubernetes-rhel/recipes/etcd.rb
+++ kubernetes-rhel/recipes/etcd.rb
@@ -1,22 +1,15 @@
 include_recipe 'kubernetes-rhel::repo-setup'
 
-bash 'wait_a_moment' do
-	user 'root'
-	code <<-EOH
-	yum -d0 -e0 -y install etcd
-	EOH
+package 'etcd' do
+	action :install
 end
-
-#package 'etcd' do
-#	action :nothing
-#end
 
 template '/etc/etcd/etcd.conf' do
 	source "etcd.conf.erb"
 	mode "0755"
 	owner "root"
-	#subscribes :create, "package[etcd]", :delayed
-	subscribes :create, "bash[wait_a_moment]", :delayed
+	subscribes :create, "package[etcd]", :delayed
+	#subscribes :create, "bash[wait_a_moment]", :delayed
 	notifies :start, "service[etcd]", :delayed
 end
 
