53531d8fcd2f90ccaece918e0e4ba6199430cdb3
nosus <carol_hsu@trend.com.tw>
2015-12-11 12:21:15 +0800

    change etcd status

--- kubernetes-rhel/recipes/etcd.rb
+++ kubernetes-rhel/recipes/etcd.rb
@@ -2,26 +2,27 @@ include_recipe 'kubernetes-rhel::repo-setup'
 
 bash 'wait_a_moment' do
 	user 'root'
-	cwd '/root'
 	code <<-EOH
     tries=0
     while [ $tries -lt 10 ]; do
         sleep 1
         tries=$((tries + 1))
     done	
+	yum -y update
+	yum -y install etcd
 	EOH
-	notifies :install, "package[etcd]", :delayed
 end
 
-package 'etcd' do
-	action :nothing
-end
+#package 'etcd' do
+#	action :nothing
+#end
 
 template '/etc/etcd/etcd.conf' do
 	source "etcd.conf.erb"
 	mode "0755"
 	owner "root"
-	subscribes :create, "package[etcd]", :delayed
+	#subscribes :create, "package[etcd]", :delayed
+	subscribes :create, "bash[wait_a_moment]", :delayed
 	notifies :start, "service[etcd]", :delayed
 end
 
