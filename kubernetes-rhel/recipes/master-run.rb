f4c4e4093a3e708743a3439feafdb35e9dabedca
carol_hsu <carol_hsu@trend.com.tw>
2015-12-12 23:54:21 +0800

    modify

--- kubernetes-rhel/recipes/master-run.rb
+++ kubernetes-rhel/recipes/master-run.rb
@@ -1,6 +1,6 @@
 include_recipe 'kubernetes-rhel::master-setup'
 #include_recipe 'kubernetes-rhel::flanneld'
-include_recipe 'kubernetes::flanneld' 
+include_recipe 'kubernetes-rhel::flanneld-init' 
 #version newer than 0.5.2 won't pass etcd URL with BA
 
 # ignore this if you don't want flanneld working on master node
