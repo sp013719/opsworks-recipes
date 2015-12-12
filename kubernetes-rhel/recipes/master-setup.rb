da2c109262fd43eace5ceb1a9d64dd7d265a729a
carol_hsu <carol_hsu@trend.com.tw>
2015-12-12 17:15:49 +0800

    update master conf

--- kubernetes-rhel/recipes/master-setup.rb
+++ kubernetes-rhel/recipes/master-setup.rb
@@ -1,6 +1,7 @@
 include_recipe 'kubernetes-rhel::repo-setup'
 
 etcd_endpoint="http://root:#{node['etcd']['password']}@#{node['etcd']['elb_url']}:80"
+
 execute 'set_flanneld_CIDR_at_ETCD' do
 	command "curl -L #{etcd_endpoint}/v2/keys/coreos.com/network/config -XPUT -d value=\"{\\\"Network\\\": \\\"#{node['kubernetes']['cluster_cidr']}\\\" }\""
 end
@@ -12,7 +13,7 @@ template "/etc/kubernetes/apiserver" do
 	owner "root"
 	source "master-apiserver.conf.erb"
 	variables({
-		:etcd_server => "#{etcd_endpoint}",
+		:etcd_server => etcd_endpoint,
 		:ba_path => "/root/ba_file",
 		:cluster_cidr => node['kubernetes']['cluster_cidr'],
 		:cluster_name => "happy-k8s-cluster"
