c5ef4683307438be9aa1023cf10a6ff891b9a579
nosus <carol_hsu@trend.com.tw>
2015-12-13 20:04:48 +0800

    update docker installation

--- kubernetes-rhel/recipes/docker.rb
+++ kubernetes-rhel/recipes/docker.rb
@@ -1,4 +1,26 @@
 include_recipe 'kubernetes-rhel::repo-setup'
 
-package 'docker'
+template "/etc/yum.repos.d/docker.repo" do
+	mode "0644"
+    owner "root"
+    source "docker.repo.erb"
+    notifies :install, "package[docker-engine]", :delayed
+end
 
+package 'docker-engine' do
+	action :nothing
+	notifies :create, "template[/etc/sysconfig/docker]", :delayed
+	notifies :create, "template[/usr/lib/systemd/system/docker.service]", :delayed
+end
+
+template "/etc/sysconfig/docker" do
+	mode "0644"
+    owner "root"
+    source "docker.erb"
+end
+
+template "/etc/sysconfig/docker" do
+    mode "0644"
+    owner "root"
+    source "docker.service.erb"
+end
