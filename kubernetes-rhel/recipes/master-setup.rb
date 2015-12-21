68fa80c2c00b04f7ecd3d660328e85f2fdd4b6f1
nosus <carol_hsu@trend.com.tw>
2015-12-21 15:36:39 +0800

    master move to new tarball installation

--- kubernetes-rhel/recipes/master-setup.rb
+++ kubernetes-rhel/recipes/master-setup.rb
@@ -1,4 +1,4 @@
-include_recipe 'kubernetes-rhel::repo-setup'
+include_recipe 'kubernetes-rhel::k8s-setup'
 
 etcd_endpoint="http://root:#{node['etcd']['password']}@#{node['etcd']['elb_url']}:80"
 
@@ -6,8 +6,17 @@ execute 'set_flanneld_CIDR_at_ETCD' do
 	command "curl -L #{etcd_endpoint}/v2/keys/coreos.com/network/config -XPUT -d value=\"{\\\"Network\\\": \\\"#{node['kubernetes']['cluster_cidr']}\\\" }\""
 end
 
-package 'kubernetes-master', 'kubernetes-client'
+#package 'kubernetes-master', 'kubernetes-client'
+bash "master-file-copy" do
+	user 'root'
+	cwd '/tmp//kubernetes/server/kubernetes/server/bin'
+	code <<-EOH
+	mkdir /etc/kubernetes
+	cp kubectl kube-apiserver kube-scheduler kube-controller-manager /usr/local/bin/
+	EOH
+end
 
+# add config file of apiserver
 template "/etc/kubernetes/apiserver" do
 	mode "0755"
 	owner "root"
@@ -18,7 +27,27 @@ template "/etc/kubernetes/apiserver" do
 		:cluster_cidr => node['kubernetes']['cluster_cidr'],
 		:cluster_name => "happy-k8s-cluster"
 	})
-	subscribes :create, "package[kubernetes-master]", :delayed
+	subscribes :create, "bash[master-file-copy]", :delayed
+end
+
+# add service init files
+template "/usr/lib/systemd/system/kube-apiserver.service" do
+	mode "0644"
+	owner "root"
+	source "kube-apiserver.service.erb"
+	subscribes :create, "bash[master-file-copy]", :delayed
+end
+template "/usr/lib/systemd/system/kube-controller-manager.service" do
+	mode "0644"
+	owner "root"
+	source "kube-controller-manager.service.erb"
+	subscribes :create, "bash[master-file-copy]", :delayed
+end
+template "/usr/lib/systemd/system/kube-scheduler.service" do
+	mode "0644"
+	owner "root"
+	source "kube-scheduler.service.erb"
+	subscribes :create, "bash[master-file-copy]", :delayed
 end
 
 file "/opt/ba_file" do
