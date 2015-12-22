2c2da04758db64c2207c37b423b042b2792a8eeb
nosus <carol_hsu@trend.com.tw>
2015-12-22 19:33:57 +0800

    add latency

--- kubernetes-rhel/recipes/minion-run.rb
+++ kubernetes-rhel/recipes/minion-run.rb
@@ -5,20 +5,18 @@ include_recipe 'kubernetes-rhel::docker-engine'
 bash 'start_flanneld_and_docker' do
 	user 'root'
 	code <<-EOH
-	mkdir /run/flannel/
 	systemctl daemon-reload	
 	service flanneld start
-	systemctl daemon-reload
 	service docker start
 	EOH
-#	notifies :start, 'service[kubelet]', :delayed
-#	notifies :start, 'service[kube-proxy]', :delayed
+	notifies :start, 'service[kubelet]', :delayed
+	notifies :start, 'service[kube-proxy]', :delayed
 end
 
-#service "kubelet" do
-#	action :nothing
-#end
-#
-#service "kube-proxy" do
-#	action :nothing
-#end
+service "kubelet" do
+	action :nothing
+end
+
+service "kube-proxy" do
+	action :nothing
+end
