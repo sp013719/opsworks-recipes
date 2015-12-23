fc0ca1a9c2875d0816bab847eccae8472fa3f4e8
nosus <carol_hsu@trend.com.tw>
2015-12-23 15:38:01 +0800

    k8s update

--- kubernetes-rhel/recipes/docker.rb
+++ kubernetes-rhel/recipes/docker.rb
@@ -2,8 +2,8 @@ include_recipe 'kubernetes-rhel::repo-setup'
 
 package 'docker' do
 	action :install
-	notifies :create, "template[/etc/sysconfig/docker]", :delayed
-	notifies :run, "bash[add-flanneld-in-docker]", :delayed
+	notifies :create, "template[/etc/sysconfig/docker]", :immediately
+	notifies :run, "bash[add-flanneld-in-docker]", :immediately
 end
 
 template "/etc/sysconfig/docker" do
@@ -12,14 +12,11 @@ template "/etc/sysconfig/docker" do
 	source "docker.erb"
 	action :nothing
 end
-#insert environment file after docker network environment file
-bash 'add-flanneld-in-docker' do
-    user 'root'
-	cwd '/usr/lib/systemd/system'
-    code <<-EOH
-	LINE_NUM=$(sed -n "0,/docker-network/p" docker.service | wc -l)
-	sed -i "${LINE_NUM}a EnvironmentFile=-/run/flannel/docker.env" docker.service
-    EOH
+
+template "/usr/lib/systemd/system/docker.service" do
+    mode "0644"
+    owner "root"
+    source "docker.service.erb"
     action :nothing
 end
 
